2b3ab7ecba03a16780b21ef04e7ae0c0a3af0c03
Fleron98 <fredrik.fleron@gmail.com>
2025-07-11 22:38:06 +0200

    fix 10

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,4 +3,4 @@ import os
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-sys.path.insert(0, os.path.join(project_root,'src/mypkg'))
+sys.path.insert(0, os.path.join(project_root, 'src/mypkg'))
